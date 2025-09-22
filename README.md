ecfe3e7fc1e5fb4858ac18578ad4f228492460c6
nhattvim <nhattruong13112000@gmail.com>
2025-09-22 14:06:48 +0700

    change from spring-boot-cli to spring api
    you don't need to install spring-boot-cli anymore

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 - [Java](https://www.java.com/)
 - [Maven](https://maven.apache.org/)
 - [Gradle](https://gradle.org/)
-- [Spring-boot-cli](https://spring.io/projects/spring-boot)
 
 ### Description
 
-A folk of [pojokcodeid/auto-java-project.nvim](https://github.com/pojokcodeid/auto-java-project.nvim)
+Inspired by [pojokcodeid/auto-java-project.nvim](https://github.com/pojokcodeid/auto-java-project.nvim)
 
 ### Installation
 
--- lua/java_project/springboot.lua
+++ lua/java_project/springboot.lua
@@ -1,5 +1,52 @@
 local U = require("java_project.utils")
 
+-------------------------------------------------
+-- Helper: URL Encode
+-------------------------------------------------
+local function urlencode(str)
+    if str == nil then
+        return ""
+    end
+    str = str:gsub("\n", "\r\n")
+    str = str:gsub("([^%w%-_%.~])", function(c)
+        return string.format("%%%02X", string.byte(c))
+    end)
+    return str
+end
+
+-------------------------------------------------
+-- Helper: Capitalize
+-------------------------------------------------
+local function capitalize(str)
+    return (str and #str > 0) and (str:sub(1, 1):upper() .. str:sub(2)) or str
+end
+
+-------------------------------------------------
+-- Helper: Parse Boot Version
+-------------------------------------------------
+local function parse_boot_version(ver)
+    -- 1. if .RELEASE -> remove .RELEASE
+    if ver:match("%.RELEASE$") then
+        return ver:gsub("%.RELEASE$", "")
+    end
+
+    -- 2. if .BUILD%-SNAPSHOT -> change to -SNAPSHOT
+    if ver:match("%.BUILD%-SNAPSHOT$") then
+        return ver:gsub("%.BUILD%-SNAPSHOT$", "-SNAPSHOT")
+    end
+
+    -- 3. if .M%d+ -> change to -M%d+
+    if ver:match("%.M%d+$") then
+        return ver:gsub("%.(M%d+)$", "-%1")
+    end
+
+    -- 4. else -> do nothing
+    return ver
+end
+
+-------------------------------------------------
+-- Dependency Selection
+-------------------------------------------------
 local function select_dependencies(dep_metadata)
     local categories = {}
     local category_map = {}
@@ -50,11 +97,36 @@ local function select_dependencies(dep_metadata)
     return table.concat(selected_deps, ",")
 end
 
+-------------------------------------------------
+-- Download & Extract Spring Boot Project
+-------------------------------------------------
+local function download_project(url, target_dir)
+    local zip_path = target_dir .. "/starter.zip"
+
+    vim.fn.system({ "curl", "-L", "-o", zip_path, url })
+    if vim.v.shell_error ~= 0 then
+        U.notify("❌ Failed to download starter.zip", "error")
+        return false
+    end
+
+    vim.fn.system({ "unzip", "-o", zip_path, "-d", target_dir })
+    if vim.v.shell_error ~= 0 then
+        U.notify("❌ Failed to unzip starter.zip", "error")
+        return false
+    end
+
+    vim.fn.delete(zip_path)
+    return true
+end
+
+-------------------------------------------------
+-- Main Function
+-------------------------------------------------
 local function springboot_new_project()
     -------------------------
     -- Step 0: Check Requirements
     -------------------------
-    if not U.check_requirements({ "curl", "spring" }) then
+    if not U.check_requirements({ "curl", "unzip" }) then
         return
     end
 
@@ -107,57 +179,49 @@ local function springboot_new_project()
     local languages = get_options(metadata.language)
     local java_versions = get_options(metadata.javaVersion)
     local packagings = get_options(metadata.packaging)
+    local boot_versions = get_options(metadata.bootVersion)
 
-    local group_id, group_canceled = U.input("Enter Group ID: ", "com.example")
-    if group_canceled then
+    local group_id = U.input("Enter Group ID: ", "com.example")
+    if not group_id then
         return
     end
 
     local default_artifact = U.basename(project_dir)
-    local artifact_id, artifact_canceled = U.input("Enter Artifact ID: ", default_artifact)
-    if artifact_canceled or artifact_id == nil then
+    local artifact_id = U.input("Enter Artifact ID: ", default_artifact)
+    if not artifact_id then
         return
     end
 
-    local name, name_canceled = U.input("Enter project name: ", artifact_id)
-    if name_canceled or name == nil then
+    local name = U.input("Enter project name: ", artifact_id)
+    if not name then
         return
     end
 
     local default_package = group_id .. "." .. artifact_id:gsub("%-", "")
-    local package_name, package_canceled = U.input("Enter package name: ", default_package)
-    if package_canceled or package_name == nil then
+    local package_name = U.input("Enter package name: ", default_package)
+    if not package_name then
         return
     end
 
-    local description, desc_canceled = U.input("Enter project description: ", "Demo project for Spring Boot")
-    if desc_canceled then
+    local description = U.input("Enter project description: ", "Demo project for Spring Boot")
+    if not description then
         return
     end
 
-    local build_idx, build_canceled = U.select_choice("Build type: ", build_choices, "maven")
-    if build_canceled then
-        return
-    end
+    local build_idx = U.select_choice("Build type: ", build_choices, "maven")
     local build_choice = build_choices[build_idx]
 
-    local lang_idx, lang_canceled = U.select_choice("Language: ", languages, metadata.language.default)
-    if lang_canceled then
-        return
-    end
+    local lang_idx = U.select_choice("Language: ", languages, metadata.language.default)
     local language = languages[lang_idx]
 
-    local java_idx, java_canceled = U.select_choice("Java version: ", java_versions, metadata.javaVersion.default)
-    if java_canceled then
-        return
-    end
+    local java_idx = U.select_choice("Java version: ", java_versions, metadata.javaVersion.default)
     local java_version = java_versions[java_idx]
 
-    local pack_idx, pack_canceled = U.select_choice("Packaging: ", packagings, metadata.packaging.default)
-    if pack_canceled then
-        return
-    end
+    local pack_idx = U.select_choice("Packaging: ", packagings, metadata.packaging.default)
     local packaging = packagings[pack_idx]
+
+    local boot_idx = U.select_choice("Spring Boot version: ", boot_versions, metadata.bootVersion.default)
+    local boot_version = parse_boot_version(boot_versions[boot_idx])
 
     local dependencies = select_dependencies(metadata.dependencies)
     if dependencies == "" then
@@ -166,39 +230,29 @@ local function springboot_new_project()
     end
 
     -------------------------
-    -- Step 4: Run Spring CLI Command
+    -- Step 4: Build URL
     -------------------------
-    local build_flag, type_flag
-    if build_choice == "gradle" then
-        build_flag = "gradle"
-        type_flag = "gradle-project"
-    else
-        build_flag = "maven"
-        type_flag = "maven-project"
-    end
 
-    local spring_cli_cmd = string.format(
-        'spring init --java-version=%s --dependencies=%s --groupId=%s --artifactId=%s --name=%s --package-name=%s --description="%s" --language=%s --build=%s --type=%s --packaging=%s %s',
-        java_version,
-        dependencies,
-        group_id,
-        artifact_id,
-        name,
-        package_name,
-        description,
-        language,
-        build_flag,
-        type_flag,
-        packaging,
-        artifact_id
+    local type_flag = (build_choice == "gradle") and "gradle-project" or "maven-project"
+
+    local url = string.format(
+        "https://start.spring.io/starter.zip?type=%s&language=%s&bootVersion=%s&baseDir=%s&groupId=%s&artifactId=%s&name=%s&description=%s&packageName=%s&packaging=%s&javaVersion=%s&dependencies=%s",
+        urlencode(type_flag),
+        urlencode(language),
+        urlencode(boot_version),
+        urlencode(artifact_id),
+        urlencode(group_id),
+        urlencode(artifact_id),
+        urlencode(name),
+        urlencode(description),
+        urlencode(package_name),
+        urlencode(packaging),
+        urlencode(java_version),
+        urlencode(dependencies)
     )
 
-    U.notify("Running command, this may take a moment...\n" .. spring_cli_cmd, "info")
-
-    local output = vim.fn.systemlist(spring_cli_cmd)
-    local full_output = table.concat(output, "\n")
-    if vim.v.shell_error ~= 0 or not string.find(full_output, "Project extracted to") then
-        U.notify("Failed to create Spring Boot project:\n" .. full_output, "error")
+    U.notify("Downloading project...\n" .. url, "info")
+    if not download_project(url, vim.fn.fnamemodify(project_dir, ":h")) then
         return
     end
 
@@ -214,13 +268,6 @@ local function springboot_new_project()
         vim.cmd("NvimTreeRefresh")
     end
 
-    local function capitalize(str)
-        if str == nil or #str == 0 then
-            return str
-        end
-        return str:sub(1, 1):upper() .. str:sub(2)
-    end
-
     local pth = package_name:gsub("%.", "/")
     local app_name = capitalize(name:gsub("%-", "")) .. "Application." .. language
     local main_class_path = string.format("src/main/java/%s/%s", pth, app_name)
@@ -231,7 +278,7 @@ local function springboot_new_project()
         U.notify("Main class not found at: " .. main_class_path, "warn")
     end
 
-    U.notify("Spring Boot project created successfully!", "info")
+    U.notify("✅ Spring Boot project created successfully!", "info")
 end
 
 vim.api.nvim_create_user_command("NewSpringBootProject", springboot_new_project, {})
